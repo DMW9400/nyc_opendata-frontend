e95dc248e93a371d159806bf7cd2e4bb7c99a961
github username <github email address>
2018-10-01 15:59:14 -0400

    successfully fetching regions data

---
+++ src/APIs/fetches.js
@@ -0,0 +1,8 @@
+// const Fetches = {
+//   geoFetch : () => {
+//     return fetch('http://localhost:3000/metrics')
+//       .then(res => res.json())
+//       .then(json => ()=console.log(json))
+//   }
+//
+// }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Polyline } from 'react-google-maps';
+import {Fetches} from '../APIs/fetches'
 class Map extends Component {
+  componentDidMount() {
+
+    fetch('http://localhost:3000/regions')
+    .then(res => res.json())
+    .then((resJson) => {
+        console.log(resJson)
+      })
+    }
+
+        // then((response) => response.json())
+        // .then((responseJson) => {
+        //     console.log(responseJson);
+        // })
   render() {
 
     let pathCoordinates = [{lat:40.7831, lng: -73.9712},
