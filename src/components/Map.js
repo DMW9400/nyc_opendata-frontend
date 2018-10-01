a4debb64e1dd6c6be0bb2abecd2fcc529d27f0f1
github username <github email address>
2018-10-01 17:19:20 -0400

    correctly receiving geoJSON object now

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,23 +7,43 @@ class Map extends Component {
   }
   componentDidMount() {
 
+    let UpperWest = this.state.regions
+    console.log(this.state.regions[31])
+
     fetch('http://localhost:3000/regions')
     .then(res => res.json())
     .then((resJson) => {
         this.setState({
           regions: resJson
-          }, ()=>console.log(this.state)
+        }, () => this.convertHash()
+        // , ()=>console.log(this.state.regions[31].geoJSON)
         )
       })
     }
-    
+
+    convertHash = () => {
+      let region1 = this.state.regions[31]
+      let geoJ = this.state.regions[31].geoJSON
+
+      let jObject = JSON.parse(geoJ)
+
+
+      console.log('geoJ: ', jObject.geometry.coordinates[0][0][0])
+      jObject.geometry.coordinates[0]
+
+    }
+
   render() {
 
-    let pathCoordinates = [{lat:40.7831, lng: -73.9712},
-      {lat:50.1, lng:1.1}]
+
+
+    let pathCoordinates = [{lat: 37.772, lng: -122.214},
+          {lat: 21.291, lng: -157.821},
+          {lat: -18.142, lng: 178.431},
+          {lat: -27.467, lng: 153.027}]
     const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
-        defaultCenter = { { lat: 40.7831, lng: -73.9712 } }
+        defaultCenter = {  {lat: 0, lng: -180} }
         defaultZoom = { 13 }
         >
           <Polyline
