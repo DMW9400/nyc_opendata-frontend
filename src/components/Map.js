59cfc664b501e4cf667d2860d7259fdc32d6d8f6
github username <github email address>
2018-10-01 20:10:34 -0400

    tracing all boundaries now

--- src/components/Map.js
+++ src/components/Map.js
@@ -34,42 +34,57 @@ class Map extends Component {
       return coordArr
     }
 
+    renderNames() {
+      return (
+        <h2>{this.state.regions.map(region =>  region.name)}</h2>
+      )
+      // this.state.regions.map(region =>{
+      //
+      //   return (<h2>region.name</h2>)
+      // })
 
-    renderNames = () => {
-      let pathCoordinates = [{lat:40.7831, lng: -73.9712},
-        {lat:50.1, lng:1.1}]
-      if (this.state.latLong){
-        console.log('renderNames', this.state.latLong[0])
-        let otherArr = []
-        otherArr.push(this.state.latLong[0])
-        otherArr.push(this.state.latLong[1])
-        console.log('Arr' ,otherArr)
-        return <Polyline
-          path={{lat: 40.806707, lng: -73.964849}}
-          options={{
-          strokeColor: '#00ffff',
-          strokeOpacity: 1,
-          strokeWeight: 2,
-          icons: [{
-            icon: "hello",
-            offset: '0',
-            repeat: '10px'
-          }],
-          }}
-          draggable = {true}
-      />
-      }
+
+      // return <h2>THIS HERE</h2>
+    }
+
+
+    renderRegions(){
+        return this.state.regions.map(regionJ => {
+          let region = JSON.parse(regionJ.geoJSON)
+          let coordinates = region.geometry.coordinates[0][0]
+          let coordArr = []
+          coordinates.map(coordinate => coordArr.push({lat:coordinate[1], lng:coordinate[0]} ))
+          console.log('cord arr', coordArr)
+          return (
+            <Polyline
+            path={coordArr}
+            options={{
+              strokeColor: '#fc1e0d',
+              strokeOpacity: 1,
+              strokeWeight: 2,
+              icons: [{
+                icon: "hello",
+                offset: '0',
+                repeat: '10px'
+              }],
+            }}
+            draggable = {true}
+          />
+        )
+
+          // console.log(region.geometry.coordinates[0][0])
+        });
     }
 
   render() {
     let pathCoordinates = this.state.latLong
     const GoogleMapExample = withGoogleMap(props => (
-
       <GoogleMap
         defaultCenter = { { lat: 40.7831, lng: -73.9712 } }
         defaultZoom = { 13 }
         >
           <Polyline
+            name='THISONE'
             path={pathCoordinates}
             options={{
             strokeColor: '#fc1e0d',
@@ -83,7 +98,7 @@ class Map extends Component {
             }}
             draggable = {true}
         />
-        {/* {this.renderNames()} */}
+          {this.renderRegions()}
         </GoogleMap>
       ));
       return(
@@ -93,6 +108,7 @@ class Map extends Component {
             mapElement={ <div style={{ height: `100%` }} /> }
           />
           {this.renderNames()}
+
         </div>
       );
     }
